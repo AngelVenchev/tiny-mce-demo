979c495e672724bdf04b66a1583f44e64260730d
Diana Kostova <dkostova92@yahoo.com>
2019-04-27 21:28:13 +0300

    Get template content as html

--- src/App.js
+++ src/App.js
@@ -4,34 +4,45 @@ import './App.css';
 import { Editor } from '@tinymce/tinymce-react';
 
 class App extends Component {
-constructor() {
-  super();
+  constructor() {
+    super();
 
-  var templateContent = `
-    Днес, {$date} в гр. {$city} се подписа настоящият договор за сътрудничество и съвместна дейност между: фирма "{$companyName}", представлявана от {$companyOwner}, с търговски адрес {$address}, от една страна и фирма {$companyName2}, представлявана от {$companyOwner2}, с търговски адрес {$address2}, от друга страна. 
-    <p>СТРАНИТЕ СЕ СПОРАЗУМЯХА ЗА СЛЕДНОТО:</p>`
+    this.editor = null;
+    this.printContent = this.printContent.bind(this);
+    this.initEditor = this.initEditor.bind(this);
+    var templateContent = `
+      Днес, {$date} в гр. {$city} се подписа настоящият договор за сътрудничество и съвместна дейност между: фирма "{$companyName}", представлявана от {$companyOwner}, с търговски адрес {$address}, от една страна и фирма {$companyName2}, представлявана от {$companyOwner2}, с търговски адрес {$address2}, от друга страна. 
+      <p>СТРАНИТЕ СЕ СПОРАЗУМЯХА ЗА СЛЕДНОТО:</p>`
 
-  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-  var values = {
-    date: new Date().toLocaleDateString('bg-BG', options),
-    city: "Пазарджик",
-    companyName: "Венчев Консултинг ЕООД",
-    companyOwner: "Ангел Венчев",
-    companyName2: "САРА Консулт",
-    companyOwner2: "Стефка Венчева",
-    address: "ул. Янтра 14, ет 4, ап. 8",
-    address2: "ул. Янтра 14, ет 4, ап. 8"
+    var values = {
+      date: new Date().toLocaleDateString('bg-BG', options),
+      city: "Пазарджик",
+      companyName: "Венчев Консултинг ЕООД",
+      companyOwner: "Ангел Венчев",
+      companyName2: "САРА Консулт",
+      companyOwner2: "Стефка Венчева",
+      address: "ул. Янтра 14, ет 4, ап. 8",
+      address2: "ул. Янтра 14, ет 4, ап. 8"
+    }
+
+    this.init = {
+      plugins: "template",
+      // menubar: "insert",
+      // toolbar: "template",
+      templates: [{title: 'Договор', description: 'Някакъв си договор', content: templateContent}],
+      template_replace_values: values
+    }
   }
 
-  this.init = {
-    plugins: "template",
-    // menubar: "insert",
-    // toolbar: "template",
-    templates: [{title: 'Договор', description: 'Някакъв си договор', content: templateContent}],
-    template_replace_values: values
+  printContent() {
+    console.log("Template: ", this.editor.getContent())
   }
-}
+
+  initEditor(event, ref) {
+    this.editor = ref;
+  }
 
   render() {
     return (
@@ -40,7 +51,11 @@ constructor() {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Editor apiKey='hwjgvgigz3ijbarzkmw8081gddz0o1ll29tgrninsan0ocs2' init={this.init} />
+        <Editor
+          apiKey='hwjgvgigz3ijbarzkmw8081gddz0o1ll29tgrninsan0ocs2' 
+          init={this.init}
+          onInit={this.initEditor}/>
+        <button onClick={this.printContent}>Print Template</button>
       </div>
     );
   }
